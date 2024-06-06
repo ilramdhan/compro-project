46947399012002c6d1a46b7a118157e8f8b62ec6
IlhamR <ilhamram332@gmail.com>
2024-06-07 02:39:29 +0700

    Initial homepage with tailwind css done, but still the home.blade.php still in default directory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
